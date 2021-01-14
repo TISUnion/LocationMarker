3a76c9d49e32fc5bd4701e38547be00c4fe07e06
Fallen_Breath <yqx791125833@gmail.com>
2021-01-15 02:35:50 +0800

    MCDR v1.0 support and MCDR v1.0+ only

--- LocationMarker.py
+++ LocationMarker.py
@@ -1,19 +1,44 @@
 import collections
+import json
 import os
-import sys
 from threading import RLock
-from typing import List
+from typing import List, Callable, Any
 
-try:
-	from plugins.command_builder import *
-except ModuleNotFoundError:
-	sys.path.append('plugins')
-	# noinspection PyUnresolvedReferences
-	from command_builder import *
-from utils.rtext import *
+from mcdreforged.api.all import *
+
+
+PLUGIN_METADATA = {
+	'id': 'location_marker',
+	'version': '1.0.0',
+	'name': 'Location Marker',
+	'description': 'A server side waypoint manager',
+	'author': 'Fallen_Breath',
+	'link': 'https://github.com/TISUnion/LocationMarker',
+	'dependencies': {
+		'mcdreforged': '>=1.0.0-alpha.8',
+	}
+}
 
 Point = collections.namedtuple('Point', 'x y z')
 Location = collections.namedtuple('Location', 'name description dimension position')
+
+PREFIX = '!!loc'
+ITEM_PER_PAGE = 10
+HELP_MESSAGE = '''
+--------- MCDR 路标插件 v20210101 ---------
+一个位于服务端的路标管理插件
+§7{0}§r 显示此帮助信息
+§7{0} list §6[<可选页号>]§r 列出所有路标
+§7{0} search §b<关键字> §6[<可选页号>]§r 搜索坐标，返回所有匹配项
+§7{0} add §b<路标名称> §e<x> <y> <z> <维度id> §6[<可选注释>]§r 加入一个路标
+§7{0} add §b<路标名称> §ehere §6[<可选注释>]§r 加入自己所处位置、维度的路标
+§7{0} del §b<路标名称>§r 删除路标，要求全字匹配
+其中：
+当§6可选页号§r被指定时，将以每{1}个路标为一页，列出指定页号的路标
+§b关键字§r以及§b路标名称§r为不包含空格的一个字符串，或者一个被""括起的字符串
+§e维度id§r参考: 主世界为§e0§r, 下界为§e-1§r, 末地为§e1§r
+'''.strip().format(PREFIX, ITEM_PER_PAGE)
+STORAGE_FILE_PATH = os.path.join('config', PLUGIN_METADATA['id'], 'locations.json')
 
 
 class LocationStorage:
@@ -106,32 +131,14 @@ class LocationStorage:
 		return ret
 
 
-PREFIX = '!!loc'
-ITEM_PER_PAGE = 10
-HELP_MESSAGE = '''
---------- MCDR 路标插件 v20201231 ---------
-一个位于服务端的路标管理插件
-§7{0}§r 显示此帮助信息
-§7{0} list §6[<可选页号>]§r 列出所有路标
-§7{0} search §b<关键字> §6[<可选页号>]§r 搜索坐标，返回所有匹配项
-§7{0} add §b<路标名称> §e<x> <y> <z> <维度id> §6[<可选注释>]§r 加入一个路标
-§7{0} add §b<路标名称> §ehere §6[<可选注释>]§r 加入自己所处位置、维度的路标
-§7{0} del §b<路标名称>§r 删除路标，要求全字匹配
-其中：
-当§6可选页号§r被指定时，将以每{1}个路标为一页，列出指定页号的路标
-§b关键字§r以及§b路标名称§r为不包含空格的一个字符串，或者一个被""括起的字符串
-§e维度id§r参考: 主世界为§e0§r, 下界为§e-1§r, 末地为§e1§r
-'''.strip().format(PREFIX, ITEM_PER_PAGE)
-STORAGE_FILE_PATH = os.path.join('plugins', 'LocationMarker', 'locations.json')
-
 storage = LocationStorage(STORAGE_FILE_PATH)
 
 
-def show_help(server, info):
-	server.reply(info, HELP_MESSAGE)
+def show_help(source: CommandSource):
+	source.reply(HELP_MESSAGE)
 
 
-def print_location(server, info, location):
+def print_location(location, printer: Callable[[RTextBase], Any]):
 	x = location.position.x
 	y = location.position.y
 	z = location.position.z
@@ -146,7 +153,7 @@ def print_location(server, info, location):
 		-1: 'advancements.nether.root.title',
 		1: 'advancements.end.root.title'
 	}
-	server.reply(info, RTextList(
+	printer(RTextList(
 		'§7-§r ',
 		name_text,
 		' ',
@@ -159,7 +166,15 @@ def print_location(server, info, location):
 	))
 
 
-def list_locations(server, info, keyword=None, page=None):
+def reply_location(source: CommandSource, location):
+	print_location(location, lambda msg: source.reply(msg))
+
+
+def broadcast_location(server: ServerInterface, location):
+	print_location(location, lambda msg: server.say(msg))
+
+
+def list_locations(source: CommandSource, *, keyword=None, page=None):
 	matched_locations = []
 	for loc in storage.get_locations():
 		if keyword is None or loc.name.find(keyword) != -1 or (loc.description is not None and loc.description.find(keyword) != -1):
@@ -167,12 +182,12 @@ def list_locations(server, info, keyword=None, page=None):
 	matched_count = len(matched_locations)
 	if page is None:
 		for loc in matched_locations:
-			print_location(server, info, loc)
+			reply_location(source, loc)
 	else:
 		left, right = (page - 1) * ITEM_PER_PAGE, page * ITEM_PER_PAGE
 		for i in range(left, right):
 			if 0 <= i < matched_count:
-				print_location(server, info, matched_locations[i])
+				reply_location(source, matched_locations[i])
 
 		has_prev = 0 < left < matched_count
 		has_next = 0 < right < matched_count
@@ -184,124 +199,108 @@ def list_locations(server, info, keyword=None, page=None):
 		if has_next:
 			next_page.c(RAction.run_command, '{} list {}'.format(PREFIX, page + 1)).h('点击显示下一页')
 
-		server.reply(info, RTextList(
+		source.reply(RTextList(
 			prev_page,
 			' 第§6{}§r页 '.format(page),
 			next_page
 		))
 	if keyword is None:
-		server.reply(info, '共有§6{}§r个路标'.format(matched_count))
+		source.reply('共有§6{}§r个路标'.format(matched_count))
 	else:
-		server.reply(info, '共找到§6{}§r个路标'.format(matched_count))
+		source.reply('共找到§6{}§r个路标'.format(matched_count))
 
 
-def add_location(server, info, name, x, y, z, dim, desc=None):
+def add_location(source: CommandSource, name, x, y, z, dim, desc=None):
 	if storage.contains(name):
-		server.reply(info, '路标§b{}§r已存在，无法添加'.format(name))
+		source.reply('路标§b{}§r已存在，无法添加'.format(name))
 		return
 	try:
 		location = Location(name, desc, dim, Point(x, y, z))
 		storage.add(location)
 	except Exception as e:
-		server.reply(info, '路标§b{}§r添加§c失败§r: {}'.format(name, e))
+		source.reply('路标§b{}§r添加§c失败§r: {}'.format(name, e))
 	else:
-		server.reply(info, '路标§b{}§r添加§a成功'.format(name))
-		print_location(server, info, location)
+		source.get_server().say('路标§b{}§r添加§a成功'.format(name))
+		broadcast_location(source.get_server(), location)
 
 
-def add_location_here(server, info, name, desc=None):
-	if not info.is_player:
-		server.reply(info, '仅有玩家允许使用本指令')
+def add_location_here(source: CommandSource, name, desc=None):
+	if not isinstance(source, PlayerCommandSource):
+		source.reply('仅有玩家允许使用本指令')
 		return
-	api = server.get_plugin_instance('PlayerInfoAPI')
-	pos = api.getPlayerInfo(server, info.player, 'Pos')
-	dim = api.getPlayerInfo(server, info.player, 'Dimension')
+	api = source.get_server().get_plugin_instance('PlayerInfoAPI')
+	pos = api.getPlayerInfo(source.player, 'Pos')
+	dim = api.getPlayerInfo(source.player, 'Dimension')
 	if type(dim) is str:  # 1.16+
 		dim = {'minecraft:overworld': 0, 'minecraft:the_nether': -1, 'minecraft:the_end': 1}[dim]
-	add_location(server, info, name, pos[0], pos[1], pos[2], dim, desc)
+	add_location(source, name, pos[0], pos[1], pos[2], dim, desc)
 
 
-def delete_location(server, info, name):
+def delete_location(source: CommandSource, name):
 	loc = storage.remove(name)
 	if loc is not None:
-		server.reply(info, '已删除路标§b{}§r'.format(name))
-		print_location(server, info, loc)
+		source.get_server().say('已删除路标§b{}§r'.format(name))
+		broadcast_location(source.get_server(), loc)
 	else:
-		server.reply(info, '未找到路标§b{}§r'.format(name))
+		source.reply('未找到路标§b{}§r'.format(name))
 
 
-class Executor:
-	def __init__(self):
-		self.server = None
-		self.info = None
-		self.executor = Literal(PREFIX). \
-			run(lambda ctx: show_help(self.server, self.info)). \
-			then(Literal('all').
-				run(lambda ctx: list_locations(self.server, self.info))
-			). \
-			then(Literal('list').
-				run(lambda ctx: list_locations(self.server, self.info)).
-				then(Integer('page').
-					run(lambda ctx: list_locations(self.server, self.info, page=ctx['page']))
+def on_load(server: ServerInterface, old_inst):
+	server.register_help_message(PREFIX, '路标管理')
+	global storage
+	if hasattr(old_inst, 'storage') and type(old_inst.storage) == type(storage):
+		storage.lock = old_inst.storage.lock
+	storage.load(server.logger)
+	server.register_command(
+		Literal(PREFIX).
+		runs(show_help).
+		then(
+			Literal('all').runs(lambda src: list_locations(src))
+		).
+		then(
+			Literal('list').runs(lambda src: list_locations(src)).
+			then(
+				Integer('page').runs(lambda src, ctx: list_locations(src, page=ctx['page']))
+			)
+		).
+		then(
+			Literal('search').then(
+				QuotableText('keyword').runs(lambda src, ctx: list_locations(src, keyword=ctx['keyword'])).
+				then(
+					Integer('page').runs(lambda src, ctx: list_locations(src, keyword=ctx['keyword'], page=ctx['page']))
 				)
-			). \
-			then(Literal('search').
-				then(QuotableText('keyword').
-					run(lambda ctx: list_locations(self.server, self.info, keyword=ctx['keyword'])).
-					then(Integer('page').
-						run(lambda ctx: list_locations(self.server, self.info, keyword=ctx['keyword'], page=ctx['page']))
+			)
+		).
+		then(
+			Literal('add').then(
+				QuotableText('name').
+				then(
+					Literal('here').
+					runs(lambda src, ctx: add_location_here(src, ctx['name'])).
+					then(
+						GreedyText('desc').runs(lambda src, ctx: add_location_here(src, ctx['name'], ctx['desc']))
 					)
-				)
-			). \
-			then(Literal('add').
-				then(QuotableText('name').
-					then(Literal('here').
-						run(lambda ctx: add_location_here(self.server, self.info, ctx['name'])).
-						then(GreedyText('desc').
-							run(lambda ctx: add_location_here(self.server, self.info, ctx['name'], ctx['desc']))
-						)
-					).
-					then(Number('x').
-						then(Number('y').
-							then(Number('z').
-								then(Integer('dim').in_range(-1, 1).
-									run(lambda ctx: add_location(self.server, self.info, ctx['name'], ctx['x'], ctx['y'], ctx['z'], ctx['dim'])).
-									then(GreedyText('desc').
-										run(lambda ctx: add_location(self.server, self.info, ctx['name'], ctx['x'], ctx['y'], ctx['z'], ctx['dim'], ctx['desc']))
-									)
+				).
+				then(
+					Number('x').then(
+						Number('y').then(
+							Number('z').then(
+								Integer('dim').in_range(-1, 1).
+								runs(lambda src, ctx: add_location(src, ctx['name'], ctx['x'], ctx['y'], ctx['z'], ctx['dim'])).
+								then(
+									GreedyText('desc').
+									runs(lambda src, ctx: add_location(src, ctx['name'], ctx['x'], ctx['y'], ctx['z'], ctx['dim'], ctx['desc']))
 								)
 							)
 						)
 					)
 				)
-			). \
-			then(Literal('del').
-				then(QuotableText('name').
-					run(lambda ctx: delete_location(self.server, self.info, ctx['name']))
-				)
 			)
-
-	def execute(self, server, info, command):
-		self.server = server
-		self.info = info
-		self.executor.execute(command)
-
-
-executor = Executor()
-
-
-def on_user_info(server, info):
-	try:
-		executor.execute(server, info, info.content)
-	except UnknownRootArgument:
-		pass
-	except CommandError as e:
-		server.reply(info, RText(e, color=RColor.red))
-
-
-def on_load(server, old_inst):
-	server.add_help_message(PREFIX, '路标管理')
-	global storage
-	if hasattr(old_inst, 'storage') and type(old_inst.storage) == type(storage):
-		storage.lock = old_inst.storage.lock
-	storage.load(server.logger)
+		).
+		then(
+			Literal('del').then(
+				QuotableText('name').
+				runs(lambda src, ctx: delete_location(src, ctx['name']))
+			)
+		)
+	)
